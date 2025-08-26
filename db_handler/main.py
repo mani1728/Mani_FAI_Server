dbd02a6624d2fc6de8f75e727aa510c39102a2a6
mani1728 <amirrezasobhi021@gmail.com>
2025-08-27 02:46:41 +0330

    Waits 30 seconds for server selection and initial connection

--- db_handler/main.py
+++ db_handler/main.py
@@ -18,7 +18,8 @@ MONGO_URI = os.environ.get("MONGO_URI")
 if not MONGO_URI:
     logging.error("MONGO_URI environment variable not set!")
 
-client = MongoClient(MONGO_URI)
+# ۳۰ ثانیه برای انتخاب سرور و اتصال اولیه صبر می‌کند
+client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=30000)
 logging.info("Successfully connected to MongoDB.")
 
 
